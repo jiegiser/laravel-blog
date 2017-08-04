32c7ca3fd0aad0805ee692a63c4968e980814345
jiegiser <cjieouba@outlook.com>
2017-08-04 23:33:17 +0800

    博客项目之后台文章分类添加模版分配及父级分类嵌入
    博客项目之后台文章分类添加模版分配及父级分类嵌入

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Model\Category;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends CommonController
+{
+    //get admin/category 全部分类列表
+    public function index()
+    {
+//        读取全部信息
+        $categorys=(new Category)->tree();
+//        return view('admin.category.index')->with('data','$categorys');
+        return view('admin.category.index', ['data' => $categorys]);
+    }
+
+    public function changeOrder()
+    {
+        $input=Input::all();
+//        echo $input['cate_id'];
+        $cate=Category::find($input['cate_id']);
+        $cate->cate_order=$input['cate_order'];
+        $re= $cate->update();
+        if($re){
+            $data=[
+                'status'=>0,
+                'msg'=>'分类排序成功！',
+            ];
+        }
+        else{
+            $data=[
+                'status'=>1,
+                'msg'=>'分类排序更新失败，请稍后重试！',
+            ];
+        }
+        return $data;
+    }
+
+//get.admin/category/create 添加分类
+    public function create()
+    {
+        $data=Category::where('cate_pid',0)->get();
+        return view('admin/category/add',compact('data'));
+    }
+    //post.admin/category 添加分类提交
+    public function store()
+    {
+//        读取input表单中的数据，除去token值之外的数据，写入数据库中。
+            $input=Input::except('_token');
+//        对用户提交的内容进限定
+            $rules=[
+//                文章的标题不能为空
+                'cate_name'=>'required',
+            ];
+            $message=[
+                'cate_name.required' =>'分类名称不能为空！',//验证required是否有值
+            ];
+            $validator=Validator::make($input,$rules,$message);
+            //引入validator服务，第一个为需要验证的参数，第二个为验证规则，第三个参数为提示信息
+            if($validator->passes()){//判断是否passes通过了返回为真
+//                将入库的数据通过category创建，然后保存在re变量中
+                $re=Category::create($input);
+                if($re){
+//                    添加数据成功，返回到分页界面
+                    return redirect('admin/category');
+                }
+                else{
+//                    添加文章失败
+                    return back()->with('errors','数据填充失败，请稍后重试！');
+                }
+            }else{
+                //若密码错误返回到修改密码窗口,
+                return back()->withErrors($validator);//传回错误信息，传回到页面,$validator参数为验证参数
+                //dd($validator->errors()->all());//找到全部的错误
+            }
+    }
+    //get.admin/category/{create}/edit 编辑分类
+    public function edit($cate_id)
+    {
+        Category::find($cate_id);
+        $data=Category::where('cate_pid',0)->get();
+        return view('admin.category.edit',compact('field','data'));
+    }
+    //put.admin/category/create 更新分类
+    public function update()
+    {
+
+    }
+    //get.admin/category/[create] 显示单个分类信息
+    public function show()
+    {
+
+    }
+    //delete.admin/category/create 删除单个分类
+    public function destory()
+    {
+
+    }
+
+}
