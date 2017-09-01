5682de43008748966d988ab6ee17927995917dfe
jiegiser <cjieouba@outlook.com>
2017-09-01 18:02:02 +0800

    前台读取数据进行展示
    前台读取数据进行展示

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Http\Model\Article;
+use App\Http\Model\Links;
+
 class IndexController extends CommonController
 {
     public function index()
     {
         //点击量最高的6篇文章（站长推荐）
-        return view('home.index');
+        $pics = Article::orderBy('art_view','desc')->take(6)->get();
+
+        //图文列表5篇（带分页）
+        $data = Article::orderBy('art_time','desc')->paginate(5);
+
+        //友情链接
+        $links = Links::orderBy('link_order','asc')->get();
+
+        return view('home.index',compact('pics','data','links'));
     }
 
     public function cate()
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,135 +1,73 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>后盾个人博客</title>
-    <meta name="keywords" content="个人博客模板,博客模板" />
-    <meta name="description" content="寻梦主题的个人博客模板，优雅、稳重、大气,低调。" />
-    <link href="{{asset('resources/views/home/css/base.css')}}" rel="stylesheet">
-    <link href="{{asset('resources/views/home/css/index.css')}}" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="{{asset('resources/views/home/js/modernizr.js')}}"></script>
-    <![endif]-->
-</head>
-<body>
-<header>
-    <div id="logo"><a href="/"></a></div>
-    <nav class="topnav" id="topnav"><a href="index.html"><span>首页</span><span class="en">Protal</span></a><a href="about.html"><span>关于我</span><span class="en">About</span></a><a href="newlist.html"><span>慢生活</span><span class="en">Life</span></a><a href="moodlist.html"><span>碎言碎语</span><span class="en">Doing</span></a><a href="share.html"><span>模板分享</span><span class="en">Share</span></a><a href="knowledge.html"><span>学无止境</span><span class="en">Learn</span></a><a href="book.html"><span>留言版</span><span class="en">Gustbook</span></a></nav>
-    </nav>
-</header>
-<div class="banner">
-    <section class="box">
-        <ul class="texts">
-            <p>打了死结的青春，捆死一颗苍白绝望的灵魂。</p>
-            <p>为自己掘一个坟墓来葬心，红尘一梦，不再追寻。</p>
-            <p>加了锁的青春，不会再因谁而推开心门。</p>
-        </ul>
-        <div class="avatar"><a href="#"><span>后盾</span></a> </div>
-    </section>
-</div>
-<div class="template">
-    <div class="box">
-        <h3>
-            <p><span>个人博客</span>模板 Templates</p>
-        </h3>
-        <ul>
-            <li><a href="/"  target="_blank"><img src="images/01.jpg"></a><span>仿新浪博客风格·梅——古典个人博客模板</span></li>
-            <li><a href="/" target="_blank"><img src="images/02.jpg"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-            <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
-            <li><a href="/" target="_blank"><img src="images/04.jpg"></a><span>女生清新个人博客网站模板</span></li>
-            <li><a href="/"  target="_blank"><img src="images/02.jpg"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-            <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
-        </ul>
+@extends('layouts.home')
+@section('info')
+    <title>{{Config::get('web.web_title')}} - {{Config::get('web.seo_title')}}</title>
+    <meta name="keywords" content="{{Config::get('web.keywords')}}" />
+    <meta name="description" content="{{Config::get('web.description')}}" />
+@endsection
+
+@section('content')
+    <div class="banner">
+        <section class="box">
+            <ul class="texts">
+                <p>洪水灾害是区域经济社会可持续发展的重要制约因素之一。</p>
+                <p>洪水灾害不仅对区域内的基础设施造成毁灭性破坏。</p>
+                <p>而且对人民群众的生命安全构成极大的威胁和损害，已经成为当前防汛抗旱中的突出问题。</p>
+            </ul>
+            <div class="avatar"><a href="#"><span>管理员</span></a> </div>
+        </section>
     </div>
-</div>
-<article>
-    <h2 class="title_tj">
-        <p>文章<span>推荐</span></p>
-    </h2>
-    <div class="bloglist left">
-        <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-        <figure><img src="images/001.png"></figure>
-        <ul>
-            <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-            <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-        </ul>
-        <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-        <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-        <figure><img src="images/001.png"></figure>
-        <ul>
-            <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-            <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-        </ul>
-        <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-        <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-        <figure><img src="images/001.png"></figure>
-        <ul>
-            <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-            <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-        </ul>
-        <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-        <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-        <figure><img src="images/001.png"></figure>
-        <ul>
-            <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-            <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-        </ul>
-        <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-        <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-        <figure><img src="images/001.png"></figure>
-        <ul>
-            <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-            <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-        </ul>
-        <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-    </div>
-    <aside class="right">
-        <div class="weather"><iframe width="250" scrolling="no" height="60" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=12&icon=1&num=1"></iframe></div>
-        <div class="news">
+    <div class="template">
+        <div class="box">
             <h3>
-                <p>最新<span>文章</span></p>
+                <p><span>预警</span>信息发布</p>
             </h3>
-            <ul class="rank">
-                <li><a href="/" title="Column 三栏布局 个人网站模板" target="_blank">Column 三栏布局 个人网站模板</a></li>
-                <li><a href="/" title="with love for you 个人网站模板" target="_blank">with love for you 个人网站模板</a></li>
-                <li><a href="/" title="免费收录网站搜索引擎登录口大全" target="_blank">免费收录网站搜索引擎登录口大全</a></li>
-                <li><a href="/" title="做网站到底需要什么?" target="_blank">做网站到底需要什么?</a></li>
-                <li><a href="/" title="企业做网站具体流程步骤" target="_blank">企业做网站具体流程步骤</a></li>
-                <li><a href="/" title="建站流程篇——教你如何快速学会做网站" target="_blank">建站流程篇——教你如何快速学会做网站</a></li>
-                <li><a href="/" title="box-shadow 阴影右下脚折边效果" target="_blank">box-shadow 阴影右下脚折边效果</a></li>
-                <li><a href="/" title="打雷时室内、户外应该需要注意什么" target="_blank">打雷时室内、户外应该需要注意什么</a></li>
-            </ul>
-            <h3 class="ph">
-                <p>点击<span>排行</span></p>
-            </h3>
-            <ul class="paih">
-                <li><a href="/" title="Column 三栏布局 个人网站模板" target="_blank">Column 三栏布局 个人网站模板</a></li>
-                <li><a href="/" title="withlove for you 个人网站模板" target="_blank">with love for you 个人网站模板</a></li>
-                <li><a href="/" title="免费收录网站搜索引擎登录口大全" target="_blank">免费收录网站搜索引擎登录口大全</a></li>
-                <li><a href="/" title="做网站到底需要什么?" target="_blank">做网站到底需要什么?</a></li>
-                <li><a href="/" title="企业做网站具体流程步骤" target="_blank">企业做网站具体流程步骤</a></li>
-            </ul>
-            <h3 class="links">
-                <p>友情<span>链接</span></p>
-            </h3>
-            <ul class="website">
-                <li><a href="http://www.houdunwang.com">后盾网</a></li>
-                <li><a href="http://bbs.houdunwang.com">后盾论坛</a></li>
+            <ul>
+                @foreach($pics as $p)
+                    <li><a href="{{url('a/'.$p->art_id)}}"  target="_blank"><img src="{{url($p->art_thumb)}}"></a><span>{{$p->art_title}}</span></li>
+                @endforeach
             </ul>
         </div>
-        <!-- Baidu Button BEGIN -->
-        <div id="bdshare" class="bdshare_t bds_tools_32 get-codes-bdshare"><a class="bds_tsina"></a><a class="bds_qzone"></a><a class="bds_tqq"></a><a class="bds_renren"></a><span class="bds_more"></span><a class="shareCount"></a></div>
-        <script type="text/javascript" id="bdshare_js" data="type=tools&amp;uid=6574585" ></script>
-        <script type="text/javascript" id="bdshell_js"></script>
-        <script type="text/javascript">
-            document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000)
-        </script>
-        <!-- Baidu Button END -->
-    </aside>
-</article>
-<footer>
-    <p>Design by 后盾网 <a href="http://www.miitbeian.gov.cn/" target="_blank">http://www.houdunwang.com</a> <a href="/">网站统计</a></p>
-</footer>
-<script src="{{asset('resources/views/home/js/silder.js')}}"></script>
-</body>
-</html>
+    </div>
+    <article class="blogs">
+        <h2 class="title_tj">
+            <p>最新<span>信息</span></p>
+        </h2>
+        <div class="bloglist left">
+            @foreach($data as $d)
+                <h3>{{$d->art_title}}</h3>
+                <figure><img src="{{url($d->art_thumb)}}"></figure>
+                <ul>
+                    <p>{{$d->art_description}}</p>
+                    <a title="{{$d->art_title}}" href="{{url('a/'.$d->art_id)}}" target="_blank" class="readmore">阅读全文>></a>
+                </ul>
+                <p class="dateview"><span>{{date('Y-m-d',$d->art_time)}}</span><span>作者：{{$d->art_editor}}</span></p>
+            @endforeach
+            <div class="page">
+                {{$data->links()}}
+            </div>
+        </div>
+        <aside class="right">
+            <!-- Baidu Button BEGIN -->
+            <div id="bdshare" class="bdshare_t bds_tools_32 get-codes-bdshare"><a class="bds_tsina"></a><a class="bds_qzone"></a><a class="bds_tqq"></a><a class="bds_renren"></a><span class="bds_more"></span><a class="shareCount"></a></div>
+            <script type="text/javascript" id="bdshare_js" data="type=tools&amp;uid=6574585" ></script>
+            <script type="text/javascript" id="bdshell_js"></script>
+            <script type="text/javascript">
+                document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000)
+            </script>
+            <!-- Baidu Button END -->
+
+            <div class="news" style="float:left;">
+                @parent
+                <h3 class="links">
+                    <p>友情<span>链接</span></p>
+                </h3>
+                <ul class="website">
+                    @foreach($links as $l)
+                        <li><a href="{{$l->link_url}}" target="_blank">{{$l->link_name}}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+
+        </aside>
+    </article>
+@endsection
